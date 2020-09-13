d63b8072ead3ce9131510c0781fbb88cd7bd0e80
closekn <close.me.kn@gmail.com>
2020-09-14 05:42:04 +0900

    Function Design for KMP

--- src/KnuthMorrisPratt.jl
+++ src/KnuthMorrisPratt.jl
@@ -1,5 +1,20 @@
 module KnuthMorrisPratt
 
-# Write your package code here.
+  #=
+    @fn     word search using KMP
+    @param  [1] (text::String) run text to search
+            [2] (word::String) search word
+    @return (Int) position in text when word is found
+  =#
+  function KMP_search(text::String, word::String) :: Int
+  end
+
+  #=
+    @fn     Generate partial match table for KMP
+    @param  (word::String) search word
+    @return (Int[]) partial match table
+  =#
+  function generate_KMP_table(word::String) :: Int[]
+  end
 
 end
