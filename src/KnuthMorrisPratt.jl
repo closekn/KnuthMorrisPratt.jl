f9022661c956ebcd12707ce9cdb70971e80b426e
closekn <close.me.kn@gmail.com>
2020-09-14 06:33:53 +0900

    [make Function] KnuthMorrisPratt.KMP_search

--- src/KnuthMorrisPratt.jl
+++ src/KnuthMorrisPratt.jl
@@ -4,9 +4,29 @@ module KnuthMorrisPratt
     @fn     word search using KMP
     @param  [1] (text::String) run text to search
             [2] (word::String) search word
-    @return (Int64) position in text when word is found
+    @return (Int64) Found     -> position of word in text
+                    Not Found -> length(text) + 1
   =#
-  function KMP_search(text::String, word::String) :: Int
+  function KMP_search(text::String, word::String) :: Int64
+    table = generate_KMP_table(word)
+    p = 0
+    i = 1
+    # search
+    while p+i <= length(text)
+      if word[i] == text[p+i]
+        i += 1
+        if i == length(word)+1
+          return p+1
+        end
+      else
+        p += i - table[i]
+        if i > 1
+          i = table[i]
+        end
+      end
+    end
+    # not found
+    return length(text)+1
   end
 
   #=
