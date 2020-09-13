b02e929ee2e7906556d7f6c4d5d194f2ce91131b
closekn <close.me.kn@gmail.com>
2020-09-14 06:58:31 +0900

    [make Test] KnuthMorrisPratt.generate_KMP_table

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using KnuthMorrisPratt
 using Test
 
 @testset "KnuthMorrisPratt.jl" begin
-    # Write your tests here.
+    # function generate_KMP_table
+    test_sample1 = KnuthMorrisPratt.generate_KMP_table("ABCDABD")
+    test_sample2 = KnuthMorrisPratt.generate_KMP_table("AAAAAAA")
+    test_sample3 = KnuthMorrisPratt.generate_KMP_table("abcabcacab")
+    test_len1 = KnuthMorrisPratt.generate_KMP_table("a")
+    test_len2 = KnuthMorrisPratt.generate_KMP_table("ab")
+    @test test_sample1 == [0, 1, 1, 1, 1, 2, 3]
+    @test test_sample2 == [0, 1, 2, 3, 4, 5, 6]
+    @test test_sample3 == [0, 1, 1, 1, 2, 3, 4, 5, 1, 2]
+    @test test_len1 == [0]
+    @test test_len2 == [0, 1]
 end
