59107d72ffa8e48100178b533dba595a74887af4
closekn <close.me.kn@gmail.com>
2020-09-14 06:08:08 +0900

    [make Function] KnuthMorrisPratt.generate_KMP_table

--- src/KnuthMorrisPratt.jl
+++ src/KnuthMorrisPratt.jl
@@ -4,7 +4,7 @@ module KnuthMorrisPratt
     @fn     word search using KMP
     @param  [1] (text::String) run text to search
             [2] (word::String) search word
-    @return (Int) position in text when word is found
+    @return (Int64) position in text when word is found
   =#
   function KMP_search(text::String, word::String) :: Int
   end
@@ -12,9 +12,26 @@ module KnuthMorrisPratt
   #=
     @fn     Generate partial match table for KMP
     @param  (word::String) search word
-    @return (Int[]) partial match table
+    @return (Array{Int64, 1}) partial match table
   =#
-  function generate_KMP_table(word::String) :: Int[]
+  function generate_KMP_table(word::String) :: Array{Int64, 1}
+    table = ones(Int64, length(word))
+    table[1] = 0
+    i = 3
+    j = 1
+    while i <= length(word)
+      if word[i-1] == word[j]
+        table[i] = j+1
+        i += 1
+        j += 1
+      elseif j > 1
+        j = table[j]
+      else
+        table[i] = 1
+        i += 1
+      end
+    end
+    return table
   end
 
 end
